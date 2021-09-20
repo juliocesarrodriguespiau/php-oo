85ebbd4c98024ed6516c0ff66bd1242dd4266d85
Júlio César Rodrigues Piau <juliocrpiau80@gmail.com>
2021-09-20 02:13:11 -0300

    commit-new-pdo-repository

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 require_once 'autoload.php';
 
 use Julio\Comercial\Infraestrutura\CriadorConexao;
+use Julio\Comercial\Infraestrutura\Repository\PdoRepositorioProduto;
 use Julio\Comercial\Model\Produto;
 
 echo "<pre>";
 
-    $conexao = CriadorConexao::criarConexao();
-    var_dump($conexao);
+    $repositorio = new PdoRepositorioProduto(CriadorConexao::criarConexao());
+    var_dump($repositorio);
 
     $produto1 = new Produto(NULL, "Tablet", 3000.12);
     var_dump($produto1);
